1bcba65d3219cc77dc40b6ad8306becc0175287c
Dan Hill <dan@danhill.us>
2014-11-24 10:47:16 -0600

    added  substring comparison function, not yet tested

--- http_request.c
+++ http_request.c
@@ -9,10 +9,28 @@ int http_request_type(char *request){
         type_str[i] = request[i];
     }
 
-    if(strcmp(type_str, "GET") == 0) {
+    if(strcmp(type_str, "GET") == 0){
         return REQ_TYPE_GET;
     }
 
+    if(strcmp(type_str, "POST") == 0){
+        return REQ_TYPE_POST;
+    }
+}
+int http_request_path(char *request){
+    int front_pos = strcspn(request, " ");
+
+
+}
+
+int substrcmp(char* str1, char* str2, int lng){
+    int i;
+    for(i = 0; i < lng; i++){
+        if (str1 + i != str2 + i){
+            return 0;
+        }
+    }
+    return 1;
 }
 
 struct http_request parse_request(char *request){
