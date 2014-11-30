b9055eeae79ea74d98149dbd4b81d38a16d7476e
Dan Hill <dan@danhill.us>
2014-11-30 17:14:55 -0600

    abort if route is not defined with error

--- controller/routes.c
+++ controller/routes.c
@@ -1,9 +1,11 @@
 #include "routes.h"
 #include "index/index.h"
 void router(int* client_sd, struct http_request_parser* req, struct http_response* res){
-    printf("%i", *client_sd);
     if(strcmp(req->path, "/") == 0){
         index_handler(client_sd, req, res);
+        return;
     }
+
+    abort_request(client_sd, HTTP_BAD_REQUEST, "No resource at this route.");
 }
 
